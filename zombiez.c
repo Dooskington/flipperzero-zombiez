1c540cb38ad770b3c9560291d7340211f9966d09
Declan Hopkins <hopkins.declan@gmail.com>
2022-05-22 18:23:56 -0700

    got basic shooting implemented

--- zombiez.c
+++ zombiez.c
@@ -45,8 +45,12 @@ typedef struct {
     int y;
 
     Player player;
-    Zombie zombies[ZOMBIES_MAX];
-    Projectile projectiles[PROJECTILES_MAX];
+    Zombie* zombies[ZOMBIES_MAX];
+
+    size_t projectiles_count;
+    Projectile* projectiles[PROJECTILES_MAX];
+
+    bool input_shoot;
 } PluginState; 
 
 static void render_callback(Canvas* const canvas, void* ctx) {
@@ -61,17 +65,24 @@ static void render_callback(Canvas* const canvas, void* ctx) {
 
     canvas_draw_str_aligned(canvas, plugin_state->x, plugin_state->y, AlignCenter, AlignCenter, "@");
 
-    //canvas_draw_circle(canvas, 64, 20, 8);
+    for (int i = 0; i < PROJECTILES_MAX; ++i) {
+        Projectile* p = plugin_state->projectiles[i];
+        if (p != NULL) {
+            canvas_draw_circle(canvas, p->position.x, p->position.y, 3);
+        }
+    }
 
-    //canvas_draw_dot(canvas, 85, 15);
+    canvas_draw_line(canvas, WALL_X, 2, WALL_X, 100);
+    canvas_draw_line(canvas, WALL_X + 2, 4, WALL_X + 2, 100);
 
-    canvas_draw_line(canvas, WALL_X, 2, WALL_X, 126);
-    canvas_draw_line(canvas, WALL_X + 2, 4, WALL_X + 2, 124);
+    //char* info = (char*)malloc(16 * sizeof(char));
+    //asprintf(&info, "%d, %d", plugin_state->x, plugin_state->y);
+    //canvas_draw_str_aligned(canvas, 32, 16, AlignLeft, AlignBottom, info);
+    //free(info);
 
-    char* info = (char*)malloc(16 * sizeof(char));
-    asprintf(&info, "%d, %d", plugin_state->x, plugin_state->y);
-    canvas_draw_str_aligned(canvas, 32, 16, AlignLeft, AlignBottom, info);
-    free(info);
+    if (plugin_state->projectiles_count >= PROJECTILES_MAX) {
+        canvas_draw_str_aligned(canvas, 16, 16, AlignLeft, AlignCenter, "OUT OF AMMO");
+    }
 
     release_mutex((ValueMutex*)ctx, plugin_state);
 }
@@ -83,9 +94,44 @@ static void input_callback(InputEvent* input_event, osMessageQueueId_t event_que
     osMessageQueuePut(event_queue, &event, 0, osWaitForever);
 }
 
+static void tick(PluginState* const plugin_state) {
+    if (plugin_state->input_shoot && (plugin_state->projectiles_count < PROJECTILES_MAX)) {
+        Projectile* p = (Projectile*)malloc(sizeof(Projectile));
+        p->position.x = plugin_state->x;
+        p->position.y = plugin_state->y;
+
+        size_t idx = plugin_state->projectiles_count;
+        plugin_state->projectiles[idx] = p;
+        plugin_state->projectiles_count += 1;
+    }
+
+    for (int i = 0; i < PROJECTILES_MAX; ++i) {
+        Projectile* p = plugin_state->projectiles[i];
+        if (p != NULL) {
+            p->position.x += 3;
+
+            if (p->position.x >= 128) {
+                free(p);
+                plugin_state->projectiles[i] = NULL;
+            }
+        }
+    }
+
+    plugin_state->input_shoot = false;
+}
+
+static void timer_callback(osMessageQueueId_t event_queue) {
+    furi_assert(event_queue);
+
+    PluginEvent event = {.type = EventTypeTick};
+    osMessageQueuePut(event_queue, &event, 0, 0);
+}
+
 static void hello_world_state_init(PluginState* const plugin_state) {
     plugin_state->x = PLAYER_START_X;
     plugin_state->y = PLAYER_START_Y;
+
+    plugin_state->projectiles_count = 0;
 } 
 
 int32_t zombiez_app(void* p) { 
@@ -106,30 +152,34 @@ int32_t zombiez_app(void* p) {
     view_port_draw_callback_set(view_port, render_callback, &state_mutex);
     view_port_input_callback_set(view_port, input_callback, event_queue);
  
+    osTimerId_t timer =
+        osTimerNew(timer_callback, osTimerPeriodic, event_queue, NULL);
+    osTimerStart(timer, osKernelGetTickFreq() / 22);
+
     // Open GUI and register view_port
     Gui* gui = furi_record_open("gui"); 
     gui_add_view_port(gui, view_port, GuiLayerFullscreen); 
 
     PluginEvent event; 
-    for(bool processing = true; processing;) { 
+    bool isRunning = true;
+    while(isRunning) { 
         osStatus_t event_status = osMessageQueueGet(event_queue, &event, NULL, 100);
         PluginState* plugin_state = (PluginState*)acquire_mutex_block(&state_mutex);
-
         if(event_status == osOK) {
             if(event.type == EventTypeKey) {
                 if(event.input.type == InputTypePress) {  
                     switch(event.input.key) {
                     case InputKeyUp: 
-                            plugin_state->y--;
+                         plugin_state->y--;
                         break; 
                     case InputKeyDown: 
-                            plugin_state->y++;
+                        plugin_state->y++;
                         break; 
                     case InputKeyOk: 
-                        // todo shoot
+                        plugin_state->input_shoot = true;
                         break;
                     case InputKeyBack: 
-                        processing = false;
+                        isRunning = false;
                         break;
                     default:
                         break;
@@ -138,15 +188,17 @@ int32_t zombiez_app(void* p) {
                 else if(event.input.type == InputTypeRepeat) {  
                     switch(event.input.key) {
                     case InputKeyUp: 
-                            plugin_state->y -= 2;
+                         plugin_state->y -= 2;
                         break; 
                     case InputKeyDown: 
-                            plugin_state->y += 2;
+                        plugin_state->y += 2;
                         break; 
                     default:
                         break;
                     }
                 }
+            } else if(event.type == EventTypeTick) {
+                tick(plugin_state);
             }
         } else {
             FURI_LOG_D("zombiez", "osMessageQueue event timeout");
